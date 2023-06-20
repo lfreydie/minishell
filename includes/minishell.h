38ffd7c7607359cc7731cdd4dde623f5c6ac356e
lefreydier <lefreydier@LAPTOP-U4OF5VH2>
2023-06-20 15:51:24 +0200

    init project

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lefreydier <lefreydier@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/20 15:42:55 by lefreydier        #+#    #+#             */
+/*   Updated: 2023/06/20 15:48:04 by lefreydier       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
