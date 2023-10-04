856fc3c35cf06e69470dee08f456d3bbd77e22c6
blandine berthod <blandineberthod@macstudblandine.home>
2023-10-04 11:50:24 +0200

    redir

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: blandineberthod <blandineberthod@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:06:10 by blandineber       #+#    #+#             */
-/*   Updated: 2023/10/03 19:03:31 by blandineber      ###   ########.fr       */
+/*   Updated: 2023/10/04 11:49:58 by blandineber      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,17 +46,33 @@ void	add_token(t_data *data, t_tok *token)
 	data->num_tokens++;
 }
 
+
+// > = 1, >> = 2, < = 3, << = 4
 int	search_redirections(t_data *data, char *token)
 {
 	int	i;
+	int	j;
 
 	i = 0;
+	j = 0;
 	while (token[i])
 	{
-		if (token[i] == '>')
-			redir[0] = 0;
+		if (token[i] == '>' || token[i] == '<')
+		{
+			data->redir[j] = (int *)malloc(sizeof(int));
+			if (token[i] == '>' && token[i + 1] != '>')
+				*data->redir[j] = 1;
+			else if (token[i] == '>' && token[i + 1] == '>')
+				*data->redir[j] = 2;
+			else if (token[i] == '<' && token[i + 1] != '<')
+				*data->redir[j] = 3;
+			else if (token[i] == '<' && token[i + 1] == '<')
+				*data->redir[j] = 4;
+			j++;
+		}
+		i++;
 	}
-	return (0);
+	return (j);
 }
 
 void	parse_token(t_data *data)
@@ -77,21 +93,3 @@ void	parse_token(t_data *data)
 		id++;
 	}
 }
-
-// void	parse_token(t_data *data)
-// {
-// 	int		id;
-// 	char	**cmd;
-// 	char	*redir_in;
-// 	char	*redir_out;
-
-// 	id = 0;
-// 	redir_in = NULL;
-// 	redir_out = NULL;
-// 	while (data->tokens[id])
-// 	{
-// 		cmd = ft_split(data->tokens[id], ' ');
-// 		add_token(data, create_token(id, cmd, redir_in, redir_out));
-// 		id++;
-// 	}
-// }
