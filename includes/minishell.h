339f91a5a2e6ef7879625b55745fb226a2e2e2a6
bberthod <bberthod@student.42.fr>
2023-10-26 14:21:48 +0000

    free_part

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lefreydier <lefreydier@student.42.fr>      +#+  +:+       +#+        */
+/*   By: bberthod <bberthod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 15:42:55 by lefreydier        #+#    #+#             */
-/*   Updated: 2023/10/26 15:44:05 by lefreydier       ###   ########.fr       */
+/*   Updated: 2023/10/26 14:19:34 by bberthod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,6 +105,7 @@ typedef struct s_data{
 
 //----------------INIT_FREE-------------
 void	free_t_data(t_data *data);
+void	free_part(t_data *data);
 
 //----------------MAIN------------------
 void	interrupt_handler(int signum);
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lefreydier <lefreydier@student.42.fr>      +#+  +:+       +#+        */
+/*   By: bberthod <bberthod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2023/10/26 15:07:30 by lefreydier       ###   ########.fr       */
+/*   Updated: 2023/10/26 14:19:58 by bberthod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,9 +65,10 @@ int	main(int ac, char **av)
 			return (printf("exit\n"), 0);
 		init_process(&data, input);
 		// launch_exec_process(data);
-		free_t_data(&data); // free_part(data) -> token + cmd + line + exec
-		// free(input);
+		free(input);
+		free_part(&data);
 		t_state.signal = 0;
 	}
+	free_t_data(&data);
 	return (0);
 }
